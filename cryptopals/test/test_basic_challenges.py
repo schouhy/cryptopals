4de0c15a57a511775ad8f9793f4a5df266a04a8e
Sergio Chouhy <sergio.chouhy@gmail.com>
2022-11-19 18:41:17 -0300

    add challenge 8 test

--- cryptopals/test/test_basic_challenges.py
+++ cryptopals/test/test_basic_challenges.py
@@ -71,4 +71,14 @@ def test_challenge_7():
     # Assert the expected plaintext and the solution match
     assert plaintext.decode() == expected_plaintext
 
+def test_challenge_8():
+    line_to_find = "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a"
+    # Load file
+    with open(TEST_DIR / "challenge8.txt", "r") as file:
+        lines = file.readlines()
+    lines = [crypto_base.hex_to_bytes(line.strip("\n")) for line in lines]
+    # Find the line encrypted with ECB as the one with less entropy
+    entropies = [(crypto_base.entropy(line), i) for i, line in enumerate(lines)]
+    _, argmin = sorted(entropies, key=lambda x: x[0])[0]
+    assert lines[argmin].hex() == line_to_find
 
