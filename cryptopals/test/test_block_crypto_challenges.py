82e4dfaa0994d366011a2ee232764090f625facd
Sergio Chouhy <sergio.chouhy@gmail.com>
2022-11-20 20:18:28 -0300

    add challenge 10 test

--- cryptopals/test/test_block_crypto_challenges.py
+++ cryptopals/test/test_block_crypto_challenges.py
@@ -1,7 +1,22 @@
 import cryptopals.base as crypto_base
+from pathlib import Path
 
+TEST_DIR = Path(__file__).parent
 
 def test_challenge_9():
     plaintext = b"YELLOW SUBMARINE"
     expectd_padded_plaintext = b"YELLOW SUBMARINE\x04\x04\x04\x04"
     assert crypto_base.pkcs7_padding(plaintext, 20) == expectd_padded_plaintext
+
+def test_challenge_10():
+    ciphertext = crypto_base.load_multiline_base64(TEST_DIR / "challenge10.txt")
+    key = b"YELLOW SUBMARINE"
+    initialization_vector = b"\x00"*16
+    scheme = crypto_base.AESCBC(key=key)
+    plaintext = scheme.decrypt(ciphertext, initialization_vector)
+    # Load solution
+    with open(TEST_DIR / "challenge10_plaintext.txt", "r") as solution_file:
+        expected_plaintext = solution_file.readlines()
+    expected_plaintext = "".join(expected_plaintext)
+    assert plaintext.decode() == expected_plaintext
+
