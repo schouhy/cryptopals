ebb718752a86ca26eb69db2bd1d33fd51845b808
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-04-18 10:58:43 +0000

    add challenge 17 padding oracle

---
+++ cryptopals/challenge17.py
@@ -0,0 +1,29 @@
+from collections import namedtuple
+import cryptopals.base as crypto_base
+import random
+from pathlib import Path
+
+DIR = Path(__file__).parent
+
+OracleResult = namedtuple("OracleResult", ["cookie", "iv"])
+
+
+class Oracle:
+    def __init__(self) -> None:
+        key = crypto_base.sample_random_bytes(size=16)
+        self._scheme = crypto_base.AESCBC(key=key)
+        with open(DIR / "challenge17.txt", "r") as infile:
+            self._list = infile.readlines()
+        self._list = [o.strip("\n").encode() for o in self._list]
+
+    def sample_cookie(self) -> bytes:
+        cookie = random.choice(self._list)
+        iv = crypto_base.sample_random_bytes(16)
+        cookie = crypto_base.pkcs7_pad(cookie, block_size=16)
+        return OracleResult(cookie=self._scheme.encrypt(cookie, iv)[0], iv=iv)
+
+    def check_cookie_padding(self, cookie_cipher: OracleResult) -> bytes:
+        cookie = self._scheme.decrypt(cookie_cipher.cookie, cookie_cipher.iv)
+        pad = cookie[-1]
+        return crypto_base.pkcs7_check_padding(cookie, pad)
+
